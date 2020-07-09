be4ac7ad75617b9cfdc35289174441fed6c62921
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-07-09 13:25:31 +0530

    Switch case and Monthly Wage in same Case

--- calculation1.sh
+++ calculation1.sh
@@ -7,17 +7,18 @@ IS_PART_TIME=2
 WAGE_PER_HR=20
 FULL_DAY_HR=8
 PART_TIME_HR=4
+WORKING_DAY_PER_MONTH=20
 
 att=$((RANDOM%3))
 
 case $att in 
 $IS_FULL_TIME)
-	dailyWage=$((WAGE_PER_HR*FULL_DAY_HR))
-	echo "Salary provided for a Full Time employee per day is " $dailyWage
+	monthlyWage=$((WAGE_PER_HR*FULL_DAY_HR*WORKING_DAY_PER_MONTH))
+	echo "Salary provided for a Full Time employee per day is " $monthlyWage
 		;;
 $IS_PART_TIME)
-	dailyWage=$((WAGE_PER_HR*PART_TIME_HR))
-	echo "Salary provided for a Part Time employee per day is " $dailyWage
+	monthlyWage=$((WAGE_PER_HR*PART_TIME_HR*WORKING_DAY_PER_MONTH))
+	echo "Salary provided for a Part Time employee per day is " $monthlyWage
 		;;
 *) 	
 	echo "Sorry, Employee is Absent"
