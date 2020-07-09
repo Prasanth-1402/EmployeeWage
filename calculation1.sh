c2b4e7048281c1a6b1f0abc855f227dfabc67108
Prasanth-Shanmugam <prasanthshanmugam1402@gmail.com>
2020-07-09 12:14:22 +0530

    Calculating the Daily Wage

--- calculation1.sh
+++ calculation1.sh
@@ -3,13 +3,15 @@
 #CONSTANTS
 IS_PRESENT=1
 IS_ABSENT=0
-
+WAGE_PER_HR=20
+FULL_DAY_HR=8
 
 att=$((RANDOM%2))
 
 if (($att==$IS_PRESENT))
 then
-	echo "Present"
+	dailyWage=$((WAGE_PER_HR*FULL_DAY_HR))
+	echo "Salary provided per is " $dailyWage
 else
-	echo "Absent"
+	echo "Sorry, the Employee is Absent"
 fi
